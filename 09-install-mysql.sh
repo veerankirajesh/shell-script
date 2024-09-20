29a4979019ca1a3b5a6a7dcb5ab6777fe4ace5e3
veerankirajesh <adevops87@gmail.com>
2024-09-20 18:34:21 +0530

    added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
+
 ID=$(id -u)
+
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: please run this script with root access"
+    echo "ERROR:: Please run this script with root access"
     exit 1 # you can give other than 0
 else
-    echo "you are root user"
-fi # fi means conditions end
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
 yum install mysql -y
+
 if [ $? -ne 0 ]
 then
-    echo "ERROR:: installing mysql is faild"
+    echo "ERROR:: Installing MySQL is failed"
     exit 1
 else
-    echo "installing mysql is success"
+    echo "Installing MySQL is SUCCESS"
 fi
+
 yum install git -y
+
 if [ $? -ne 0 ]
-then 
-    echo "ERROR:: installing git faild"
+then
+    echo "ERROR:: Installing GIT is failed"
     exit 1
 else
-    echo "installing git success"
-fi                 
+    echo "Installing GIT is SUCCESS"
+fi
