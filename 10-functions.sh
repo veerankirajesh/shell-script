5063072c0c6b59834684e4f6d59ef5f7f8ef5517
veerankirajesh <adevops87@gmail.com>
2024-09-20 19:12:49 +0530

    added 2nd script

--- 10-functions.sh
+++ 10-functions.sh
@@ -2,21 +2,22 @@
 ID=$(id -u)
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo "ERROR:: $2 ..FAILDED"
+    then
+        echo "ERROR:: $2 ... FAILED"
         exit 1
-    else    
-        echo "$2 ..SUCCESS"
-    fi    
+    else
+        echo "$2 ... SUCCESS"
+    fi
 }
-if [ $ID -ne 0]
+
+if [ $ID -ne 0 ]
 then
-    echo "ERROR:: please run this script with root access"
-    exit 1
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
 else
-    echo "you are root user"
-fi
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
 yum install mysql -y
-VALIDATE $? "installing mysql"
+VALIDATE $? "Installing MySQL"
 yum install git -y
-VALIDATE $? "installing git"    
+VALIDATE $? "Installing GIT"
