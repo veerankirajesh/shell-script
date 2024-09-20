7c88929169d2cf74636fffa6e10dc4ca97c7bf2c
veerankirajesh <adevops87@gmail.com>
2024-09-20 19:10:34 +0530

    added 2nd script

--- 10-functions.sh
+++ 10-functions.sh
@@ -5,7 +5,18 @@ VALIDATE(){
     then 
         echo "ERROR:: $2 ..FAILDED"
         exit 1
-    ele    
+    else    
         echo "$2 ..SUCCESS"
-
+    fi    
 }
+if [ $ID -ne 0]
+then
+    echo "ERROR:: please run this script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
+yum install mysql -y
+VALIDATE $? "installing mysql"
+yum install git -y
+VALIDATE $? "installing git"    
