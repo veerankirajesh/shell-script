00485a761f9b970118cdbbab890c27b17f836dd6
veerankirajesh <adevops87@gmail.com>
2024-09-21 12:44:48 +0530

    added 2nd script

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,9 +1,7 @@
-
 #!/bin/bash
-NUMBER=$1
-if [ $NUMBER -gt 100 ]
-then
-   echo "Give number $NUMBER is greater than 100"
+NUMBER=$20
+fi [ $NUMBER -gt 100 ]
+then 
+    echo "Given number is $NUMBER is greaterthen 100"
 else
-   echo "Give number $NUMBER is not greater than 100"
-fi
+    echo "Given number is $NUMBER is not greaterthen 100"    
