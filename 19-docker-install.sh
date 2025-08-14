e5c40e6186ad27c228b59729e80e9ed3e233780d
veerankirajesh <adevops87@gmail.com>
2025-08-14 15:29:35 +0530

    docker1

--- 19-docker-install.sh
+++ 19-docker-install.sh
@@ -6,58 +6,60 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/${0##*/}-$TIMESTAMP.log"
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" &>> "$LOGFILE"
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
 
-VALIDATE() {
-    if [ $1 -ne 0 ]; then
-        echo -e "$2 ... ${R}FAILED${N}"
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
         exit 1
     else
-        echo -e "$2 ... ${G}SUCCESS${N}"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-if [ $ID -ne 0 ]; then
-    echo -e "${R}ERROR:: Please run this script with root access${N}"
-    exit 1
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
 else
     echo "You are root user"
-fi
+fi # fi means reverse of if, indicating condition end
 
-# Remove old Docker versions
-dnf remove -y docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine &>> "$LOGFILE"
+yum install -y yum-utils
 
-# Install required plugin tools
-dnf install -y dnf-plugins-core &>> "$LOGFILE"
-VALIDATE $? "Installed dnf plugins core"
+VALIDATE $? "Installed yum utils"
 
-# Add Docker repository for RHEL 9
-dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &>> "$LOGFILE"
-VALIDATE $? "Added Docker repo"
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-# Install Docker
-dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &>> "$LOGFILE"
-VALIDATE $? "Installed Docker components"
+VALIDATE $? "Added docker repo"
 
-# Start and enable Docker
-systemctl enable --now docker &>> "$LOGFILE"
-VALIDATE $? "Started and enabled Docker"
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
-# Detect username for Docker group
-CURRENT_USER=${SUDO_USER:-$(logname)}
-usermod -aG docker "$CURRENT_USER" &>> "$LOGFILE"
-VALIDATE $? "Added $CURRENT_USER to Docker group"
+VALIDATE $? "Installed docker components"
 
-# Install latest Docker Compose
-COMPOSE_VERSION="2.24.6"
-curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" \
-    -o /usr/local/bin/docker-compose &>> "$LOGFILE"
+systemctl start docker
+
+VALIDATE $? "Started docker"
+
+systemctl enable docker
+
+VALIDATE $? "Enabled docker"
+
+usermod -aG docker centos
+
+VALIDATE $? "added centos user to docker group"
+
+curl -L https://github.com/docker/compose/releases/download/2.24.6/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
 chmod +x /usr/local/bin/docker-compose
-docker compose version &>> "$LOGFILE"
-VALIDATE $? "Installed Docker Compose"
 
-# Apply docker group immediately (no need to logout)
-exec sg docker newgrp `id -gn`
+docker compose version
+
+VALIDATE $? "Installed docker compose"
+
+echo -e "$R Logout and login again $N"
